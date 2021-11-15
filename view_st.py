91cd2fe5bf2e5b304aba1748be55582a5c15ebe9
Gustavo Despaigne <gusta230799@gmail.com>
2021-11-14 22:33:20 -0500

    added selectbox

--- view_st.py
+++ view_st.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
+import pandas
+
 st.title("Aqui va un titulo")
 uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+
+metodo = st.selectbox("Elija el metodo que desea utilizar", ["Booleno", "Vectorial", "Probabilistico"])
