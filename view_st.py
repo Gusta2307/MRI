02ff8d829b73b71ee2ff359986c7a5f3284e2d05
Gustavo Despaigne <gusta230799@gmail.com>
2021-12-31 00:22:05 -0500

    visual listo

--- view_st.py
+++ view_st.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from doc_bd import Documents
+from herramienta import metodo_booleano
 
 def get_info(document):
     info = ""
@@ -16,31 +17,53 @@ def get_info(document):
 
 st.title("Sistema de Recuperacion de Informacion")
 # uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
-df = open("./ej_pdf1.json")
+df = open("./ej_pdf.json")
 document = Documents(df)
 
 
-info_document = get_info(document)
-st.code(info_document)
+# info_document = get_info(document)
+# st.code(info_document)
 
+st.subheader("Terminos indexados")
+
+str_terms = ''
+temp_index = 0
+for terms in document.terms:
+    str_terms += str(terms).ljust(25)
+    temp_index += 1
+    if temp_index == 3:
+        str_terms += '\n'
+        temp_index = 0
+
+st.text(str_terms)
 
 metodo = st.selectbox("Elija el metodo que desea utilizar:", ["Booleano", "Vectorial", "Probabilistico"])
 
-st.text(document.terms)
 query = st.text_input("Inserte la consulta.", "")
 
 str_result = ""
 if st.button("Submit") and query != "":
     if metodo == "Booleano":
-        result, doc_ok = document.metodo_booleano(query)
+        result, doc_ok, term_omitidos = metodo_booleano(document, query)
         st.subheader("Output:")
         str_result = "Evaluacion de la consulta por documentos:\n\n"
         for item in result:
             str_result += str(item) + "\n"
-        str_result += "\nDocumentos recuperados: \n"
-        for item in doc_ok:
-            str_result += str(item) + "\n"
         st.code(str_result)
+        str_result = ''
+        if doc_ok:
+            str_result += "\nDocumentos recuperados: \n"
+            for item in doc_ok:
+                str_result += str(item) + "\n"
+            st.success(str_result)
+        else:
+            st.error("No se recupero ningun documento")
+        str_term =''
+        if term_omitidos:
+            str_term += f"Esta consulta contiene los siguientes terminos que pueden ser considerados irrelevantes, lo cual puede afectar el resultaddo de la busqueda de manera desfavorable \n"
+            for term in term_omitidos:
+                str_term += '- ' + str(term) + "\n"
+            st.warning(str_term)
     elif metodo == "Vectorial":
         result = document.metodo_vectorial(query)
         str_result += "\nDocumentos recuperados: \n"
