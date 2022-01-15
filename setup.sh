be2474b2bb343945f5ab443cca87d2a4a3b22393
Gustavo Despaigne <gusta230799@gmail.com>
2022-01-15 01:13:10 -0500

    terminos visual ok3

--- setup.sh
+++ setup.sh
@@ -6,8 +6,4 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-<<<<<<< HEAD
-" > ~/.streamlit/config.toml 
-=======
 " > ~/.streamlit/config.toml
->>>>>>> fa89e2506872edba4f269e372f24b3959b2679a4
