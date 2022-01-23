f5fb5bcb837a3f74980fc74fdc0d24f65547b6e1
Gustavo Despaigne <gusta230799@gmail.com>
2022-01-23 14:46:04 -0500

    listo

--- testing.py
+++ testing.py
@@ -3,15 +3,15 @@ from doc_bd import Documents
 from motor_de_busqueda import *
 from datetime import datetime
 
-text = open("Test Collections/adi/adi_data.json")
-text_prep = open("Test Collections/adi/adi_data_prep.json")
-text_raiz = open("Test Collections/adi/adi_data_prep_raiz.json")
+text = open("Test Collections/cisi/cisi_data.json")
+text_prep = open("Test Collections/cisi/cisi_data_prep.json")
+text_raiz = open("Test Collections/cisi/cisi_data_prep_raiz.json")
 
 doc = Documents(text, text_prep, text_raiz)
 
 querys = json.loads(open("Test Collections/adi/adi_query_bln.json").read())
 
-result = json.loads(open("Test Collections/adi/result_ADI.json").read())
+result = json.loads(open("Test Collections/cisi/result_CISI.json").read())
 
 # print(len(querys.keys()))
 
@@ -28,8 +28,15 @@ def recobrado(RR, NR):
     except:
         return 0
 
+def f1(p,r):
+    try:
+      return 2/((1/p) + (1/r))
+    except:
+        return 0
+
 pre = 0
 re = 0
+_f1_pre = 0
 index = 0
 cant_rr = 0
 time = 0
@@ -64,14 +71,18 @@ for q in querys.keys():
         # print(f"RR: {RR}\nRI: {RI}\n" )
         p =  precision(RR, RI)
         r =  recobrado(RR, NR)
+        _f1 = f1(p,r)
         pre += p
         re += r
+        _f1_pre += _f1
 
         print("precision", p)
         print("recobrado", r) 
+        print("F1", _f1)
 
 print("\nPrecision promedio", pre/index)
 print("recobrado promedio", re/index)
+print("F1 promedio", _f1_pre/index)
 print("RR promedio", cant_rr/index)
 print("Time", time/index)
 
